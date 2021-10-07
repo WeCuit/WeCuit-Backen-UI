2fde9ba19d21ba7443d5025271b82f88d4988abb
jiyeme <jiyecafe@gmail.com>
2021-10-07 08:06:40 +0000

    Deploying to gh-pages from  @ 79d9f2d95b0fbca73d5a4205b9729f78930f5ee9 🚀

---
+++ assets/index-legacy.32f4f6c0.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.304c10b1.js"],(function(n){"use strict";var t;return{setters:[function(n){t=n.a}],execute:function(){n("g",(function(){return t.get("/mini/index-slide")})),n("u",(function(n){return t.patch("/mini/index-slide",n)})),n("a",(function(){return t.get("/mini/config/other")})),n("p",(function(n){return t.put("/mini/config/other",n)}))}}}));
